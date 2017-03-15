9837b06343aa30d77923c61747ca736b9c37a241
frunnel <f.runnel@khk.ee>
2017-03-15 15:32:53 +0200

    koostatud rakenduse konfiguratsiooni esmaversioon

---
+++ conf.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: frunnel
+ * Date: 15.03.2017
+ * Time: 15:24
+ */
+//vajalike konstantide defineerimine
+define('CLASSES_DIR', 'classes/'); //classes kataloogi nime konstand
+define('TMPL_DIR', 'tmpl/'); //tmpl kataloogi konstant
+?>
--- index.php
+++ index.php
@@ -4,4 +4,8 @@
  * User: frunnel
  * Date: 15.03.2017
  * Time: 13:10
- */?>
+ */
+require_once 'conf.php';
+//conf.php faili kasutamine
+echo '<h1>Veebiprogrammerimine</h1>';
+?>
