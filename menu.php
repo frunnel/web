245b6b5b6ea00748b0ef580edb6d1d5557aed00b
web <f.runnel@khk.ee>
2017-03-22 15:32:05 +0200

    loodud menu esmaversioon

---
+++ menu.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: frunnel
+ * Date: 22.03.2017
+ * Time: 15:31
+ */
+// loome menüü mallide objektid
+$menu = new template('menu.menu');
+$item = new template('menu.item');
+// kontrollime objekti olemasolu ja sisu
+echo '<pre>';
+ print_r($menu);
+ print_r($item);
+ echo '<pre>';
+ ?>
