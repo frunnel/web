62ae2337efb20143785b27831ecaf4e174208d53
web <f.runnel@khk.ee>
2017-03-22 15:21:11 +0200

    site_title väärtus lisatud

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ $main_tmpl -> set('title', 'Pealeht');
 $main_tmpl -> set('lang_bar', 'Keeleriba');
 $main_tmpl -> set('menu', 'Lehe peamenüü');
 $main_tmpl -> set('content', 'Lehe sisu');
+$main_tmpl -> set('site_title', 'Veebiprogrammerimise kursus');
 //kontrollime objekti sisu
 echo $main_tmpl->parse();
 ?>
