cf67770d0a023bca4e1d96bb3973f9d9b9a36fb8
web <f.runnel@khk.ee>
2017-03-22 12:20:50 +0200

    koostatud esmaversioon template classist

---
+++ classes/template.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: frunnel
+ * Date: 22.03.2017
+ * Time: 12:11
+ */
+class template
+{
+var $file = "";//
+    var $content = false; //sisu puudub seega false
+    var $vars = array(); //html.........
+}
