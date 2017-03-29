ca7b1ba9c789d7876134877c479c3130dde7625b
web <f.runnel@khk.ee>
2017-03-29 14:38:12 +0300

    Lisatud põhilingi loomise võimalused

--- classes/linkobject.php
+++ classes/linkobject.php
@@ -7,5 +7,18 @@
  * Time: 14:35
  */
 class linkobject extends http
-{// klassi algus
-}// klassi lõpp}
+{
+    // klassi algus
+    // klassi muutujad - omadused
+    var $baseUrl = false;
+    var $delim = '&amp;';
+    var $eq = '=';
+    var $protocol = 'http://';
+    // klassi meetodid
+    // klassi konstruktor
+    function __construct(){
+        parent::__construct(); // kutsume tööle http konstruktor
+        // loome põhilink
+        $this->baseUrl = $this->protocol.HTTP_HOST.SCRIPT_NAME;
+    }// konstruktor
+}// klassi lõpp
