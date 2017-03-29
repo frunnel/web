1dd0b930026ef3c18363439b48e9be99eb3fa45b
web <f.runnel@khk.ee>
2017-03-29 13:37:43 +0300

    lisatud menüü väljastamine pealehele

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
  * Date: 15.03.2017
  * Time: 13:10
  */
+//võtan konfigu kasutusele
 require_once 'conf.php';
 //conf.php faili kasutamine
 echo '<h1>Veebiprogrammerimine</h1>';
@@ -15,6 +16,8 @@ $main_tmpl -> set('user', 'Kasutajanimi');
 $main_tmpl -> set('title', 'Pealeht');
 $main_tmpl -> set('lang_bar', 'Keeleriba');
 $main_tmpl -> set('menu', 'Lehe peamenüü');
+// kutsume menüü tööle testimiseks
+require_once 'menu.php';
 $main_tmpl -> set('content', 'Lehe sisu');
 $main_tmpl -> set('site_title', 'Veebiprogrammerimise kursus');
 //kontrollime objekti sisu
