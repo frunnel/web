7014534bd081a2cf4e2cad27c90c61c302a6d476
web <f.runnel@khk.ee>
2017-03-29 15:12:04 +0300

    Lisatud andmepaari lisamine lingile

--- classes/linkobject.php
+++ classes/linkobject.php
@@ -23,10 +23,11 @@ class linkobject extends http
     }// konstruktor
     // andmete paari koostamine kujul
 	// nimi=väärtus&nimi1=väärtus1 jne
-	function addToLink($link, $nimi, $val){
+	function addToLink(&$link, $name, $val){
     		if($link != ''){
         			$link = $link.$this->delim;
         		}
- 		$link = $link.
+ 		$link = $link.fixUrl($name).$this->eq.fixUrl($val);
+        		echo $link;
      	}// addToLink
 }// klassi lõpp
