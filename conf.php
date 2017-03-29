6a8a6ab0e50e65ba9fa2a3148105df647ec6ca03
web <f.runnel@khk.ee>
2017-03-29 14:56:26 +0300

    Lisatud lib kataloogis utils kasutamine

--- conf.php
+++ conf.php
@@ -8,11 +8,15 @@
 //vajalike konstantide defineerimine
 define('CLASSES_DIR', 'classes/'); //classes kataloogi nime konstand
 define('TMPL_DIR', 'tmpl/'); //tmpl kataloogi konstant
-//võtame kasutusel vajalikud failid
+define('LIB_DIR', 'lib/'); // lib kataloogi nime konstant
+//võtame kasutusele vajalikud abifailid
+require_once LIB_DIR.'utils.php';
+//võtame kasutusele vajalikud failid
 require_once CLASSES_DIR.'template.php';
 require_once CLASSES_DIR.'http.php';
+require_once CLASSES_DIR.'linkobject.php';
 // loome vajalikud objektid projekti tööks
-$http = new http();
+$http = new linkobject();
 // testime linkobjecti tööd
 echo $http->baseUrl;
 //echo '<pre>';
