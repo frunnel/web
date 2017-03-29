157db579a52391da8d2d7def9d8daf890ad9a4e1
web <f.runnel@khk.ee>
2017-03-29 15:36:22 +0300

    Menüü on täeindatud linkidega

--- menu.php
+++ menu.php
@@ -9,10 +9,20 @@
 $menu = new template('menu.menu');
 $item = new template('menu.item');
 // lisame sisu
+// nimetame menüüs väljastav element
 $item->set('name', 'esimene');
+// loome antud menüü elemendile lingi
+$link = $http->getLink(array('act'=>'first'));
+// lisame antud link menüüsse
+$item->set('link', $link);
+// lisame valmis link menüü objekti sisse
 $menu->set('items', $item->parse());
+//
 $item->set('name', 'teine');
+$link = $http->getLink(array('act'=>'second'));
+$item->set('link', $link);
 $menu->add('items', $item->parse());
+// kontrollime objekti olemasolu ja sisu
 // kui soovime pidevat asendamist, siis set funktsioon add asemel
 $main_tmpl->add('menu', $menu->parse());
 ?>
