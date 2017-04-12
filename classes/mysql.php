a2a6116ea084e542d0dd83d23db3021da77756f8
Felix <f.runnel@khk.ee>
2017-04-12 08:59:12 +0300

    Koostatud mysql klassi algversioon

---
+++ classes/mysql.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Felix
+ * Date: 12.04.2017
+ * Time: 8:58
+ */
+class mysql
+{
+    // klassi algus
+    // klassi omadused
+    var $conn = false; // ühendus andmebaasiserveriga
+    var $host = false; // andmebaasi serveri host
+    var $user = false; // andmebaasi serveri kasutaja
+    var $pass = false; // andmebaasi serveri parool
+    var $dbname = false; // andmebaasi serveris andmebaas
+    // klassi tegevused
+    function __construct($h, $u, $p, $dn)
+    {
+        $this->host = $h;
+        $this->user = $u;
+        $this->pass = $p;
+        $this->dbname = $dn;
+        $this->connect();
+    }// konstruktor
+    function connect()
+    {
+        $this->conn = mysqli_connect($this->host, $this->user, $this->pass, $this->dbname);
+        if(mysqli_connect_error()
+        ){
+            echo 'Viga andmebaasiserveriga ühenduses<br />';
+            exit;
+        }
+    }
+    // connect
+}
+// klassi lõpp
+?>
