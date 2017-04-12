655256d90ca8dc38c20a7eb0ddfb3d8e6958ba32
Felix <f.runnel@khk.ee>
2017-04-12 09:03:00 +0300

    Koostatud päringu põhifunktsionaal

--- conf.php
+++ conf.php
@@ -13,10 +13,17 @@ define('ACTS_DIR', 'acts/'); // acts kataloogi nime konstant
 define('DEFAULT_ACT', 'default'); // vaikimisi tegevuse faili nime konstant
 //võtame kasutusele vajalikud abifailid
 require_once LIB_DIR.'utils.php';
+require_once 'db_conf.php'; // loeme andmebaasi konfi sisse
 //võtame kasutusele vajalikud failid
 require_once CLASSES_DIR.'template.php';
 require_once CLASSES_DIR.'http.php';
 require_once CLASSES_DIR.'linkobject.php';
+require_once CLASSES_DIR.'mysql.php';
 // loome vajalikud objektid projekti tööks
 $http = new linkobject();
+$db = new mysql(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+// testime andmebaasi objekti
+echo '<pre>';
+print_r($db);
+echo '</pre>';
 ?>
