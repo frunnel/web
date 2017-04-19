e478c1ee798e4579d4b0a1b4e5adf1d629d5afff
web <f.runnel@khk.ee>
2017-04-19 09:03:50 +0300

    db uuendus

--- conf.php
+++ conf.php
@@ -9,16 +9,22 @@
 define('CLASSES_DIR', 'classes/'); //classes kataloogi nime konstand
 define('TMPL_DIR', 'tmpl/'); //tmpl kataloogi konstant
 define('LIB_DIR', 'lib/'); // lib kataloogi nime konstant
+define('ACTS_DIR', 'acts/'); // acts kataloogi nime konstant
+define('DEFAULT_ACT', 'default'); // vaikimisi tegevuse faili nime konstant
 //võtame kasutusele vajalikud abifailid
 require_once LIB_DIR.'utils.php';
+require_once 'db_conf.php'; // loeme andmebaasi konfi sisse
 //võtame kasutusele vajalikud failid
 require_once CLASSES_DIR.'template.php';
 require_once CLASSES_DIR.'http.php';
 require_once CLASSES_DIR.'linkobject.php';
+require_once CLASSES_DIR.'mysql.php';
 // loome vajalikud objektid projekti tööks
 $http = new linkobject();
-// testime linkobjecti tööd
-echo $http->baseUrl;
-//echo '<pre>';
-//print_r($http);
+$db = new mysql(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+// testime andmebaasi objekti
+$res = $db->query('SELECT NOW()');
+echo '<pre>';
+print_r($res);
+echo '</pre>';
 ?>
