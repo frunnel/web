59dae671a02356aa6070f739e53f1550d237ec2f
web <f.runnel@khk.ee>
2017-04-19 09:53:44 +0300

    Uuendatud lehe vaikimisi sisu väljastamine

--- acts/default.php
+++ acts/default.php
@@ -5,5 +5,23 @@
  * Date: 12.04.2017
  * Time: 8:50
  */
-$main_tmpl->set('content', 'Lehe sisu');
+// lehe id saamine ja teisendamine - andmebaasis BIGINT
+$page_id = (int)$http->get('page_id');
+// sql lause lehe sisu otsimiseks vastavalt id-le
+$sql = 'SELECT * FROM content WHERE '.
+    'content_id='.fixDb($page_id);
+// saadame päring andmebaasi sisu saamiseks
+$res = $db->getArray($sql);
+if($res === false)
+{
+    $sql = 'SELECT * FROM content WHERE '.
+        'is_first_page='.fixDb(1);
+    $res = $db->getArray($sql);
+}
+if($res !== false)
+{
+    $page = $res[0];
+    $http->set('page_id', $page['content_id']);
+    $main_tmpl->set('content', $page['content']);
+}
 ?>
