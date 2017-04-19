6ca8794a61889a6f267eea5b6adf366a2eb271db
web <f.runnel@khk.ee>
2017-04-19 09:51:16 +0300

    Koostatud menüü ehitamine andmebaasist

--- menu.php
+++ menu.php
@@ -9,19 +9,26 @@
 $menu = new template('menu.menu');
 $item = new template('menu.item');
 // lisame sisu
-// nimetame menüüs väljastav element
-$item->set('name', 'esimene');
-// loome antud menüü elemendile lingi
-$link = $http->getLink(array('act'=>'first'));
-// lisame antud link menüüsse
-$item->set('link', $link);
-// lisame valmis link menüü objekti sisse
-$menu->set('items', $item->parse());
-//
-$item->set('name', 'teine');
-$link = $http->getLink(array('act'=>'second'));
-$item->set('link', $link);
-$menu->add('items', $item->parse());
+// menüü sql lause
+$sql = 'SELECT content_id, title FROM content WHERE '.
+    'parent_id='.fixDb(0).' AND show_in_menu='.fixDb(1);
+// saame päringu tulemus
+$res = $db->getArray($sql);
+// kontrollime tulemuse sisu
+if($res != false)
+{
+    	foreach ($res as $page)
+    	{
+        // nimetame menüüs väljastav element
+        $item->set('name', $page['title']);
+        // loome antud menüü elemendile lingi
+        $link = $http->getLink(array('page_id'=>$page['content_id']));
+        // lisame antud link menüüsse
+        $item->set('link', $link);
+        // lisame valmis link menüü objekti sisse
+        $menu->add('items', $item->parse());
+        }
+}
 // kontrollime objekti olemasolu ja sisu
 // kui soovime pidevat asendamist, siis set funktsioon add asemel
 $main_tmpl->add('menu', $menu->parse());
